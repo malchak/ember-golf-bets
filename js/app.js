3ba2ceef7346ca6214076c6f9f0195365a37d9d5
Michael Malchak <michaelmalchak@gmail.com>
2014-01-06 17:46:05 -0800

    rename link header

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ App.RoundsRoute = Ember.Route.extend({
 App.RoundsController = Ember.ArrayController.extend({
 	inTheRed: false,
 	isVisible: false,
+	placeHolder: "input winnings, then hit enter",
 
 	actions: {
 		add: function(){
@@ -34,11 +35,7 @@ App.RoundsController = Ember.ArrayController.extend({
 			this.set('winnings', '');
 			this.transitionToRoute('rounds');
 		},
-		cancel: function(){
-			this.get('content').deleteRecord('round');
-			this.transitionToRoute('rounds');
-		},
-		toggle: function(){
+		toggleNew: function(){
 			this.toggleProperty('isVisible');
 		}
 	},
@@ -75,30 +72,30 @@ App.RoundsController = Ember.ArrayController.extend({
 		return this.get('losses').get('length');
 	}.property('losses'),
 
-	amountWon: function(){
+	totalAmountWon: function(){
 		return this.sum(this.get('wins').mapProperty('winnings'));
 	}.property('wins'),
 
-	amountLost: function(){
+	totalAmountLost: function(){
 		return this.sum(this.get('losses').mapProperty('winnings'));
 	}.property('@each'),
 	
 	netWinnings: function(){
-		var netAmount = this.get('amountWon') + this.get('amountLost');
+		var netAmount = this.get('totalAmountWon') + this.get('totalAmountLost');
 		if (netAmount < 0)
 			{this.set('inTheRed', true);}
 		else
 			{this.set('inTheRed', false);}
 		return netAmount;
-	}.property('amountWon', 'amountLost'),
+	}.property('totalAmountWon', 'totalAmountLost'),
 
 	avgWin: function(){
-		return parseInt(this.get('amountWon') / this.get('totalWinningRounds'));
-	}.property('amountWon', 'totalWinningRounds'),
+		return parseInt(this.get('totalAmountWon') / this.get('totalWinningRounds'));
+	}.property('totalAmountWon', 'totalWinningRounds'),
 
 	avgLoss: function(){
-		return parseInt(this.get('amountLost') / this.get('totalLosingRounds'));
-	}.property('amountLost', 'totalLosingRounds'),
+		return parseInt(this.get('totalAmountLost') / this.get('totalLosingRounds'));
+	}.property('totalAmountLost', 'totalLosingRounds'),
 
 	largestWin: function(){
 		var sortedWins = this.get('wins').mapProperty('winnings').sort();
@@ -111,17 +108,22 @@ App.RoundsController = Ember.ArrayController.extend({
 	}.property('losses'),
 
 	trending: function(){
-		var round = this.filter(function(round){return round;});
-		var allWinnings = round.mapProperty('winnings').slice(-3);
-		if (this.sum(allWinnings) > 0)
+		var allRounds = this.filter(function(round){return round;});
+		var lastThreeWinnings = allRounds.mapProperty('winnings').slice(-3);
+		if (this.sum(lastThreeWinnings) > 0)
 			{ return 'Up';}
-		else if (this.sum(allWinnings) < 0)
+		else if (this.sum(lastThreeWinnings) < 0)
 			{return 'Down';}
 		else
 			{return "Trending even"}
 	}.property('@each')
 });
 
-App.RoundView = Ember.View.extend({});
+Ember.TextField.reopen({
+	attributeBindings: ['autofocus']
+});
 
+App.AddRoundView = Ember.View.extend({
+	tagName: 'div'
+});
 
